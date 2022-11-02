c3a1df5962a781be2cffd871d3e1ac554b9bf9b6
Jacob Chlebowski <jachlebowski@wpi.edu>
2022-11-02 15:27:07 -0400

    starting

--- Assignment-IP.2/ninjase/src/App.js
+++ Assignment-IP.2/ninjase/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Model from './model/Model.js';
 
 function App() {
+  //NEW FROM CALC VIDEO
+  const [model,setmodel] = React.useState(new model());
+
+  //useeffect
+  //DO SOMETHING
+
+  //methods (takes event from onClick and does something)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        All html in here
     </div>
   );
 }
